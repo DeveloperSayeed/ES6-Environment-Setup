b8e7d4aa883b69c1480916032b565d150b407027
Developersayeed <abusayeedriday@gmail.com>
2022-03-29 19:27:14 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "./main.scss";
+import sayeed from "./sayeed.jpg";
+
+const photo = document.getElementById("photo")
+photo.setAttribute("src", sayeed)
